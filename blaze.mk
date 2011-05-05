4417f82c9424cac761d4d85d52f59a83caca19af
Vikram Pandita <vikram.pandita@ti.com>
2011-05-05 13:20:01 -0500

    [Blaze] Blaze.mk file update for blaze device
    Align Blaze.mk file in the device/ti/blaze directory to
    align with the blaze tablet.
    
    Change-Id: I116afac547cd5c02a8cd5410bb5feb9c9373ba23
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- blaze.mk
+++ blaze.mk
@@ -48,8 +48,10 @@ PRODUCT_PACKAGES += \
 
 # OpenMAX IL configuration
 TI_OMX_POLICY_MANAGER := hardware/ti/omx/system/src/openmax_il/omx_policy_manager
+#PRODUCT_COPY_FILES += \
+#        $(TI_OMX_POLICY_MANAGER)/src/policytable.tbl:system/etc/policytable.tbl \
+
 PRODUCT_COPY_FILES += \
-        $(TI_OMX_POLICY_MANAGER)/src/policytable.tbl:system/etc/policytable.tbl \
         device/ti/blaze/media_profiles.xml:system/etc/media_profiles.xml
 
 # graphics
@@ -127,14 +129,10 @@ PRODUCT_PACKAGES += \
     libskiahwdec \
     SkLibTiJpeg_Test
 
-#Overlay
+#HWC Hal
 PRODUCT_PACKAGES += \
-    overlay.omap4 \
-    overlay_test
+    hwcomposer.omap4
 
-# Alsa configuration
-PRODUCT_COPY_FILES += \
-        device/ti/blaze/asound.conf:system/etc/asound.conf 
 # Audio HAL
 PRODUCT_PACKAGES += \
     alsa.omap4
@@ -156,8 +154,8 @@ PRODUCT_COPY_FILES += \
 
 
 # Modem
-PRODUCT_PACKAGES += \
-   libaudiomodemgeneric
+#PRODUCT_PACKAGES += \
+#   libaudiomodemgeneric
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -202,26 +200,26 @@ PRODUCT_PACKAGES += \
         librs_jni \
         libomap_mm_library_jni
 
-
-# These are the hardware-specific features
-PRODUCT_COPY_FILES += \
-        device/ti/blaze/apns.xml:system/etc/apns-conf.xml \
-        frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-        frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-        packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml \
-        frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-        frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-        frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-        device/ti/blaze/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-        frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-        frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
-        frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-        frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-        frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-        frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
-
 # Pick up hdpi resources where available
 PRODUCT_LOCALES += hdpi
+
+# These are the hardware-specific features
+#KD# need to audit this and re-enable what is needed
+#PRODUCT_COPY_FILES += \
+#        device/ti/blaze/apns.xml:system/etc/apns-conf.xml \
+#        frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+#        frameworks/base/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+#        packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml \
+#        frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+#        frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+#        frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+#        device/ti/blaze/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+#        frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+#        frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+#        frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+#        frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+#        frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+#        frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml
 
 # Pick up audio package
 # no longer needed?
