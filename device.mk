b66fc77fb00fd6515ed7ec49d78352e942f8f3e4
Dan Murphy <dmurphy@ti.com>
2011-06-14 10:22:37 -0500

    [Blaze] Add sensors library for blaze
    Add Blaze sensor library plus add copy directive
    to device.mk file
    
    Change-Id: Ied4e8f894e59666712e9bfe322d467fee50877f6
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- device.mk
+++ device.mk
@@ -75,5 +75,9 @@ PRODUCT_PACKAGES += \
 # Audio HAL module
 PRODUCT_PACKAGES += audio.primary.blaze
 
+# Sensors
+PRODUCT_PACKAGES += \
+        sensors.omap4
+
 $(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
 $(call inherit-product-if-exists, vendor/ti/proprietary/omap4/ti-omap4-vendor.mk)
