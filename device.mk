e6b085e868f85f828db46ce601b5a7f996e76791
Pavan Savoy <x0099669@ti.com>
2011-08-11 16:57:38 -0500

    [Blaze]: add wpan products file
    WPAN incorporates software, firmware required to get Bluetooth, FM Radio
    working on the product.
    So instead of crowding the device/product list, creating wpan product list
    which come out of the hardware/ti/wpan.git project.
    
    Signed-off-by: Pavan Savoy <x0099669@ti.com>
    
    Patch Set 2:  No changes made -- simple cherry-pick.  Taking approvals
    and verfications from patch set 2.
    Signed-off-by: Jean Johnson <a0271255@ti.com>
    Change-Id: If4a376aea6f24e0807f2fd5a348ca93ae6fccb18

--- device.mk
+++ device.mk
@@ -109,10 +109,10 @@ PRODUCT_PACKAGES += \
 
 # BlueZ test tools & Shared Transport user space mgr
 PRODUCT_PACKAGES += \
-	uim-sysfs \
 	hciconfig \
 	hcitool
 
 $(call inherit-product, frameworks/base/build/tablet-dalvik-heap.mk)
 $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 $(call inherit-product-if-exists, vendor/ti/proprietary/omap4/ti-omap4-vendor.mk)
+$(call inherit-product-if-exists, hardware/ti/wpan/ti-wpan-products.mk)
