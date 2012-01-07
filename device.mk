e4d67d3a450db27350efa8fc9b71bb1b0e24e3b4
Hervé Fache <h-fache@ti.com>
2012-01-07 18:01:50 +0100

    device.mk: change LCD density from 240 to 320
    Change-Id: I2eb4c7d30fdf2695e4c03b642bdd4394b63c8198
    Signed-off-by: Hervé Fache <h-fache@ti.com>

--- device.mk
+++ device.mk
@@ -99,7 +99,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.opengles.version=131072
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.sf.lcd_density=240
+	ro.sf.lcd_density=320
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
