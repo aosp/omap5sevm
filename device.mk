a4f27e5be29ea2bcd4552c947fc8c472f1c0bb78
Leed Aguilar <leed.aguilar@ti.com>
2012-04-10 14:13:33 -0400

    omap-keypad: add input device configuration file
    Define omap4-keypad configuration file and avoid using
    a generic idc file
    
    The following properties have been set for this device:
    
    * device.internal
    * keyboard.orientationAware
    * keyboard.characterMap
    * keyboard.layout
    
    More information about input device configuration files:
    http://source.android.com/tech/input/input-device-configuration-files.html
    
    Change-Id: I8fb55679fb6aa6074b57a9f6ec87262e677ee94a
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,7 @@ PRODUCT_COPY_FILES := \
 	device/ti/omap5sevm/omap4-keypad.kl:system/usr/keylayout/omap4-keypad.kl \
 	device/ti/omap5sevm/qtouch-touchscreen.idc:system/usr/idc/qtouch-touchscreen.idc \
 	device/ti/omap5sevm/palmas_pwron.idc:system/usr/idc/palmas_pwron.idc \
+	device/ti/omap5sevm/omap4-keypad.idc:system/usr/idc/omap4-keypad.idc \
 	device/ti/omap5sevm/omap4-keypad.kcm:system/usr/keychars/omap4-keypad.kcm \
 	device/ti/omap5sevm/palmas_pwron.kcm:system/usr/keylayout/palmas_pwron.kcm \
         device/ti/omap5sevm/palmas_pwron.kl:system/usr/keylayout/palmas_pwron.kl \
