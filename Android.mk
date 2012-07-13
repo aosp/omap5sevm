f2dcf3703255e0db182c8d3ed93bb9deb7556df3
Dan Murphy <dmurphy@ti.com>
2012-07-13 10:22:11 -0500

    OMAP5sevm: Create a symlink at build time for HDCP
    Create a symlink to the hdcp.keys file located in a read only partition
    
    Change-Id: I0f2e905f936384fb47cf9c8a039d5463d2c8f7b3
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- Android.mk
+++ Android.mk
@@ -22,6 +22,31 @@
 
 LOCAL_PATH := $(call my-dir)
 
+# protect makefile from other boards
+# if some modules are built directly from this directory (not subdirectories),
+# their rules should be written here.
+ifeq ($(TARGET_DEVICE),omap5sevm)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := omap5sevm_hdcp_keys
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+LOCAL_MODULE_SUFFIX := -timestamp
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): HDCP_KEYS_FILE := /factory/hdcp.keys
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_VENDOR)/firmware/hdcp.keys
+$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(HDCP_KEYS_FILE)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(HDCP_KEYS_FILE) $(SYMLINK)
+	$(hide) touch $@
+endif
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
 
