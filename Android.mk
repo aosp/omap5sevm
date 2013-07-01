be01ae76995c953392ce697dd6bc3106d9279ef1
Vishal Mahaveer <vishalm@ti.com>
2013-07-01 12:54:52 -0500

    omap5sevm: do not build libs for other boards
    Protect device specific components from other boards.
    
    --
    similar to:
    commit c72e3ae53cbca1a7f5ebb1d162d8a16aacd10a2a from device/ti/jacinto6evm.git
    Signed-off-by: Andrii Beregovenko <a.beregovenko@ti.com>
    --
    
    Change-Id: I588a66a42869c05eb32286043cb2285a1358d26b
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- Android.mk
+++ Android.mk
@@ -46,8 +46,9 @@ $(LOCAL_BUILT_MODULE):
 	$(hide) rm -rf $(SYMLINK)
 	$(hide) ln -sf $(HDCP_KEYS_FILE) $(SYMLINK)
 	$(hide) touch $@
-endif
+
 # if some modules are built directly from this directory (not subdirectories),
 # their rules should be written here.
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
